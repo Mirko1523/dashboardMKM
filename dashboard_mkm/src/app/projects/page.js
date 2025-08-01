90d69a9ea438fe48e828fcbce3e40adf57e3d65e
Mirko1523 <milermirko1@gmail.com>
2025-08-01 03:42:49 -0300

    main

--- dashboard_mkm/src/app/projects/page.js
+++ dashboard_mkm/src/app/projects/page.js
@@ -7,15 +7,12 @@ import Image from "next/image"
 
 export default function Projects() {
     const proyectos = [
-        { name: "Tienda Reina", url: "https://tiendareina.vercel.app/", preview: "/previews/reina.png" },
-        { name: "Transportes JYJ", url: "https://transportesjyj.vercel.app/", preview: "/previews/transportesjyj.png" },
+        { name: "SIIM Ingenieria", url: "https://siimingenieria.com/", preview: "/previews/siim.png" },
+        { name: "FAA Agency", url: "https://faascalablesp.netlify.app/", preview: "/previews/faa.png" },
         { name: "Clahé Eventos", url: "https://claheeventos.vercel.app/", preview: "/previews/clahe.png" },
-        { name: "Eventos para Todos", url: "https://eventosparatodos.vercel.app/", preview: "/previews/eventosparatodos.png" },
         { name: "Quinta los Buhos", url: "https://quintalosbuhos.vercel.app/", preview: "/previews/quintalosbuhos.png" },
         { name: "Distribuidora Sudamerica", url: "https://distribuidorasudamerica.vercel.app/", preview: "/previews/sudamerica.png" },
         { name: "ConstruMX", url: "https://construmx.vercel.app/", preview: "/previews/construMX.png" },
-        { name: "TYA Exclusivos", url: "https://tyaexclusivos.vercel.app/", preview: "/previews/tyaexclusivos.png" },
-        { name: "Ferreteria Pasteur Luz", url: " https://pasteurluz.vercel.app/", preview: "/previews/PasteurLuz.png" },
     ]
 
 
@@ -25,10 +22,10 @@ export default function Projects() {
 
             <main className="max-w-7xl mx-auto px-4 py-8">
                 <h2 className="text-3xl font-bold mb-2 text-transparent bg-clip-text bg-gradient-to-r from-blue-950 to-indigo-900">
-                    Prototipos de Sitios Web (Bocetos Virtuales)
+                    Sitios Web creados y diseñados por nosotros
                 </h2>
                 <p className="mb-8 text-gray-600">
-                    Estos diseños conceptuales muestran la estética y funcionalidad que puedo desarrollar en producción. ¡Pronto aquí verás proyectos 100% en vivo!
+                    Estos diseños muestran la estética y funcionalidad que podemos desarrollar en producción. <strong>¿Qué esperas para tener el tuyo?</strong>
                 </p>
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -49,7 +46,7 @@ export default function Projects() {
                                 />
                             </div>
                             <div className="p-4">
-                                <h3 className="text-xl font-semibold text-gray-800 group-hover:text-blue-600 transition-colors">
+                                <h3 className="text-xl font-semibold text-blue-800 group-hover:text-blue-600 transition-colors">
                                     {p.name}
                                 </h3>
                             </div>
